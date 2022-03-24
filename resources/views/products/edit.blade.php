cd224426943f49fd4cc850939d03819d26de853e
maino1997 <sashamainardi4@gmail.com>
2022-03-24 17:14:47 +0100

    deleted null ?? operator

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -18,17 +18,17 @@
             @csrf
             <div class="my-3 ">
                 <label for="name" class="form-label">Nome del Prodotto</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ old('title', $product->name) ?? ''}}">
+                <input type="text" class="form-control" id="name" name="name" value="{{ old('title', $product->name) }}">
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
                 <input type="text" class="form-control" id="description" name="description"
-                    value="{{ old('description', $product->description) ?? ''}}">
+                    value="{{ old('description', $product->description) }}">
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
                 <input type="number" class="form-control" id="price" name="price" step="0.01"
-                    value="{{ old('price', $product->price) ?? '1'}}">
+                    value="{{ old('price', $product->price) }}">
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Immagine</label>
