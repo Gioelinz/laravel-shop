bdeab3b0ddfb96c92728cd8d782a7b4f20cf754a
Aleandro2119 <aleandro.basilico@gmail.com>
2022-03-24 16:54:56 +0100

    Add layout and style

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,8 +2,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
@@ -11,9 +10,9 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                        <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('products.index') }}">Products</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('products.index') }}">Products</a>
+                    </li>
                 </ul>
             </div>
         </div>
