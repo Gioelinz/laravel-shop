d01a8a0dc6f75aa731d30633362fbcd753874630
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-24 16:59:56 +0100

    fix width on description

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-<ul class="mt-4">
-    @foreach ($products as $product)
-    <li class="text-center">
-        <figure>
-            <img class="img-fluid mt-3 rounded-pill" src="{{ $product->image }}" alt="">
-        </figure>
-        <h2 class="pt-2 text-uppercase">{{ $product->name }}</h2>
-        <p>{{ $product->description }}</p>
-        <div class="pt-2">{{ $product->price }}</div>
-        <a href="{{ route('products.show', ['product' => $product->id]) }}">Dettagli</a>
-    </li>
-    @endforeach
-</ul>
+    <ul class="mt-4">
+        @foreach ($products as $product)
+            <li class="text-center">
+                <figure>
+                    <img class="img-fluid mt-3 rounded-pill" src="{{ $product->image }}" alt="">
+                </figure>
+                <h2 class="pt-2 text-uppercase">{{ $product->name }}</h2>
+                <p class="w-25 mx-auto">{{ $product->description }}</p>
+                <div class="pt-2">{{ $product->price }}</div>
+                <a href="{{ route('products.show', ['product' => $product->id]) }}">Dettagli</a>
+            </li>
+        @endforeach
+    </ul>
 @endsection
