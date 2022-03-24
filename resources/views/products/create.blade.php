971cb102b1e9f252acbde1141c06347a3cd18f18
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-24 16:52:13 +0100

    add show errors on form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,29 +1,40 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">  
-  <h2>Crea un nuovo prodotto</h2>
+    <div class="container">
 
-  <form  action="{{route('products.store')}}" method="POST">
-    @csrf
-    <div class="mb-3">
-      <label for="exampleInputEmail1" class="form-label">Inserisci nome</label>
-      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="name">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <h2>Crea un nuovo prodotto</h2>
+
+        <form action="{{ route('products.store') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Inserisci nome</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="name">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Inserisci descrizione</label>
+                <input type="text" class="form-control" id="exampleInputPassword1" name="description">
+            </div>
+            <div class="mb-3 ">
+                <label class="-label" for="exampleCheck1">Inserisci prezzo</label>
+                <input type="number" class="form-control" id="exampleCheck1" name="price">
+            </div>
+            <div class="mb-3 ">
+                <label class="-label" for="exampleCheck1">Inserisci link immagine</label>
+                <input type="string" class="form-control" id="exampleCheck1" name="image">
+            </div>
+            <button type="submit" class="btn btn-success">Submit</button>
+        </form>
+
     </div>
-    <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label">Inserisci descrizione</label>
-      <input type="text" class="form-control" id="exampleInputPassword1" name="description">
-    </div>
-    <div class="mb-3 ">
-        <label class="-label" for="exampleCheck1">Inserisci prezzo</label>
-      <input type="number" class="form-control" id="exampleCheck1" name="price">
-    </div>
-    <div class="mb-3 ">
-      <label class="-label" for="exampleCheck1">Inserisci link immagine</label>
-      <input type="string" class="form-control" id="exampleCheck1" name="image">
-      </div>
-    <button type="submit" class="btn btn-success">Submit</button>
-  </form>
-  
-</div>
 @endsection
