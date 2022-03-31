278b2ec9ab12f7f3f196591636e22319ca573555
Gioelinz <gioelerunza.gr@gmail.com>
2022-03-31 17:29:24 +0200

    better layout

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,27 +1,32 @@
 @extends('layouts.main')
 
 @section('content')
-    <ul class="mt-4">
-        @foreach ($products as $product)
-            <li class="text-center">
-                <figure>
-                    <img class="img-fluid mt-3 rounded-pill" src="{{ $product->image }}" alt="">
-                </figure>
-                <h2 class="pt-2 text-uppercase">{{ $product->name }}</h2>
-                <p class="w-25 mx-auto">{{ $product->description }}</p>
-                <div class="pt-2 fw-bold">€{{ $product->price }}</div>
-                <h5>
-                    <span
-                        class="badge rounded-pill bg-{{ $product->brand->color ?? 'dark' }}">{{ $product->brand->name ?? '-' }}</span>
-                </h5>
+    <div class="row">
 
-                @forelse ($product->colors as $color)
-                    <p>{{ $color->color }}</p>
-                @empty
-                    <p>Colore Sconosciuto</p>
-                @endforelse
-                <a href="{{ route('products.show', ['product' => $product->id]) }}">Dettagli</a>
-            </li>
+        @foreach ($products as $product)
+            <div class="col-4">
+                <div class="text-center">
+                    <figure>
+                        <img class="img-fluid mt-3 rounded-pill" src="{{ $product->image }}" alt="">
+                    </figure>
+                    <h2 class="pt-2 text-uppercase">{{ $product->name }}</h2>
+                    <p class="w-25 mx-auto">{{ substr($product->description, 0, 20) }}...</p>
+                    <div class="pt-2 fw-bold">€{{ $product->price }}</div>
+                    <h5>
+                        <span
+                            class="badge rounded-pill bg-{{ $product->brand->color ?? 'dark' }}">{{ $product->brand->name ?? '-' }}</span>
+                    </h5>
+
+                    @forelse ($product->colors as $color)
+                        <p>{{ $color->color }}</p>
+                    @empty
+                        <p>Colore Sconosciuto</p>
+                    @endforelse
+                    <a class="btn btn-primary"
+                        href="{{ route('products.show', ['product' => $product->id]) }}">Dettagli</a>
+                </div>
+            </div>
         @endforeach
-    </ul>
+
+    </div>
 @endsection
